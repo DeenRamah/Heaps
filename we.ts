a8769292ac6dc726dcd2c7dba2ffaa9dc5dcfe6f
Kiprotich Kipchumba <96067114+DeenRamah@users.noreply.github.com>
2024-11-12 05:47:35 +0300

    Create we.ts

---
+++ we.ts
@@ -0,0 +1,68 @@
+// heaps/MaxHeap.ts
+
+export class MaxHeap {
+  private heap: number[] = [];
+
+  private getParentIndex(i: number): number {
+    return Math.floor((i - 1) / 2);
+  }
+
+  private getLeftChildIndex(i: number): number {
+    return 2 * i + 1;
+  }
+
+  private getRightChildIndex(i: number): number {
+    return 2 * i + 2;
+  }
+
+  private swap(i: number, j: number): void {
+    [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
+  }
+
+  public insert(value: number): void {
+    this.heap.push(value);
+    this.heapifyUp();
+  }
+
+  private heapifyUp(): void {
+    let index = this.heap.length - 1;
+    while (index > 0) {
+      const parentIndex = this.getParentIndex(index);
+      if (this.heap[parentIndex] < this.heap[index]) {
+        this.swap(parentIndex, index);
+        index = parentIndex;
+      } else {
+        break;
+      }
+    }
+  }
+
+  public extractMax(): number | null {
+    if (this.heap.length === 0) return null;
+    if (this.heap.length === 1) return this.heap.pop() || null;
+
+    const max = this.heap[0];
+    this.heap[0] = this.heap.pop()!;
+    this.heapifyDown();
+    return max;
+  }
+
+  private heapifyDown(): void {
+    let index = 0;
+    while (this.getLeftChildIndex(index) < this.heap.length) {
+      let largerChildIndex = this.getLeftChildIndex(index);
+      const rightChildIndex = this.getRightChildIndex(index);
+      if (rightChildIndex < this.heap.length && this.heap[rightChildIndex] > this.heap[largerChildIndex]) {
+        largerChildIndex = rightChildIndex;
+      }
+
+      if (this.heap[index] >= this.heap[largerChildIndex]) break;
+      this.swap(index, largerChildIndex);
+      index = largerChildIndex;
+    }
+  }
+
+  public peek(): number | null {
+    return this.heap.length > 0 ? this.heap[0] : null;
+  }
+}
